33eed7e69f858cf40af188ec873d343faca908d3
blekso <blekepix@outlook.com>
2022-04-02 15:56:50 +0200

    commit

--- components/Dashboard.vue
+++ components/Dashboard.vue
@@ -1,10 +1,12 @@
 <template>
     <div class="h-full w-full">
-        <div class="m-4 p-4 gap-4 bg-white rounded-3xl p-4 w-64">
-            <DatePicker @input="(e) => selectedDate = e" class="col-span-2"/>
+        <div class="mb-16 ">
+            <div class="m-4 px-4 py-2 gap-4 bg-white rounded-3xl w-64" style="box-shadow: 0 0 2rem 0 rgb(136 152 170 / 15%);">
+                <DatePicker @input="(e) => selectedDate = e" class="col-span-2"/>
+            </div>
         </div>
         <div class="m-4 grid grid-cols-6 gap-4">
-            <DoughnutChart class="col-span-3 bg-white rounded-3xl p-12"/>
+            <DoughnutChart class="col-span-2 bg-white rounded-lg p-12"/>
         </div>
     </div>
 </template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,14 +1,5 @@
 <template>
   <v-app class="wrapper">
         <Nuxt />
-      <!-- div>
-        <EsNav />
-        <EsSidebar :width="'80%'" />
-      </div> -->
-      <!-- <EsBlokNewsletter /> -->
-      <!-- <EsFooter />
-      <client-only>
-        <CookieBanner />
-      </client-only> -->
     </v-app>
 </template>
