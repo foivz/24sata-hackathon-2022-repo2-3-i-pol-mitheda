111eb291ce04ffeb43c02fe0de662aede49d3e78
DarioSiroki <dario.siroki@gmail.com>
2022-04-03 00:50:45 +0200

    autofill

--- pages/expenses/index.vue
+++ pages/expenses/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="h-full w-full m-8 grid grid-cols-8 z-50">
     <div class="col-span-8">
-     <v-data-table
+      <v-data-table
         :headers="headers"
         :items="expenses"
         item-key="id"
@@ -9,11 +9,11 @@
         :search="search"
         :custom-filter="filterOnlyCapsText"
         show-expand
-         @click:row="(item, slot) => slot.expand(!slot.isExpanded)"
+        @click:row="(item, slot) => slot.expand(!slot.isExpanded)"
       >
-        <template v-slot:expanded-item="{item, headers}">
+        <template v-slot:expanded-item="{ item, headers }">
           <td :colspan="headers.length" style="padding: 0px">
-            <p>{{item}}</p>
+            <p>{{ item }}</p>
           </td>
         </template>
         <template v-slot:top>
@@ -38,94 +38,78 @@
         </template>
       </v-data-table>
       <v-row justify="center">
-        <v-dialog
-          v-model="dialog"
-          persistent
-          max-width="600px"
-        >
+        <v-dialog v-model="dialog" persistent max-width="600px">
           <template v-slot:activator="{ on, attrs }">
-            <v-btn
-              color="primary"
-              dark
-              v-bind="attrs"
-              v-on="on"
-            >
-              Open Dialog
+            <v-btn color="primary" dark v-bind="attrs" v-on="on">
+              Add expense
             </v-btn>
           </template>
           <v-card>
             <v-card-title>
-              <span class="text-h5">User Profile</span>
+              <span class="text-h5">Add expense</span>
+            </v-card-title>
+
+            <v-card-title>
+              <form @change="upload">
+                <input
+                  type="file"
+                  ref="getFile"
+                  style="display: none"
+                  name="thefile"
+                />
+              </form>
+              <v-btn color="blue darken-1" text @click="$refs.getFile.click()">
+                Pre-fill from invoice
+              </v-btn>
             </v-card-title>
             <v-card-text>
               <v-container>
                 <v-row>
-                  <v-col
-                    cols="12"
-                    sm="6"
-                    md="4"
-                  >
+                  <v-col cols="12" sm="6" md="4">
                     <v-text-field
                       label="Title"
                       required
                       v-model="newExpense.title"
                     ></v-text-field>
                   </v-col>
-                  <v-col
-                    cols="12"
-                    sm="6"
-                    md="4"
-                  >
+                  <v-col cols="12" sm="6" md="4">
                     <v-text-field
                       label="Merchant"
                       v-model="newExpense.merchant"
                     ></v-text-field>
                   </v-col>
-                  
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field
+                      label="Date"
+                      v-model="newExpense.date"
+                    ></v-text-field>
+                  </v-col>
                 </v-row>
                 <p v-if="newExpense.expense_items.length > 0">Items</p>
                 <v-row v-for="item in newExpense.expense_items" :key="item.id">
-                  <v-col
-                    cols="12"
-                    sm="6"
-                    md="4"
-                  >
+                  <v-col cols="12" sm="6" md="4">
                     <v-text-field
                       label="Title"
                       required
                       v-model="item.title"
                     ></v-text-field>
                   </v-col>
-                  <v-col
-                    cols="12"
-                    sm="6"
-                    md="4"
-                  >
+                  <v-col cols="12" sm="6" md="4">
                     <v-text-field
                       label="Amount"
                       v-model="item.amount"
                     ></v-text-field>
                   </v-col>
 
-                  <v-col
-                    cols="12"
-                    sm="6"
-                    md="4"
-                  >
+                  <v-col cols="12" sm="6" md="4">
                     <v-text-field
                       label="Price"
                       v-model="item.price"
                     ></v-text-field>
                   </v-col>
-
-                  
                 </v-row>
                 <v-row>
-                  <v-btn
-                    color="blue darken-1"
-                    text
-                    @click="addNewItem"
-                  >
+                  <v-btn color="blue darken-1" text @click="addNewItem">
                     New item
                   </v-btn>
                 </v-row>
@@ -134,18 +118,10 @@
             </v-card-text>
             <v-card-actions>
               <v-spacer></v-spacer>
-              <v-btn
-                color="blue darken-1"
-                text
-                @click="dialog = false"
-              >
+              <v-btn color="blue darken-1" text @click="dialog = false">
                 Close
               </v-btn>
-              <v-btn
-                color="blue darken-1"
-                text
-                @click="dialog = false"
-              >
+              <v-btn color="blue darken-1" text @click="dialog = false">
                 Save
               </v-btn>
             </v-card-actions>
@@ -153,77 +129,91 @@
         </v-dialog>
       </v-row>
     </div>
-    
   </div>
-
-  
 </template>
 
 <script>
 export default {
-  data () {
+  data() {
     return {
-      search: '',
-      calories: '',
+      search: "",
+      calories: "",
       expenses: [],
       dialog: false,
       newExpense: {
         newId: 0,
         title: "",
         merchant: "",
-        expense_items: []
-      }
-    }
+        date: "",
+        expense_items: [],
+      },
+    };
   },
   async mounted() {
     this.$nextTick(async () => {
-      console.log("TOKEN:"+this.$store.state.user.token)
-      console.log()
-      const expensesx = await this.$axios.$get('/api/expenses', {
+      console.log("TOKEN:" + this.$store.state.user.token);
+      console.log();
+      const expensesx = await this.$axios.$get("/api/expenses", {
         params: {
-          token: this.$store.state.user.token
-        }
-      })
-      this.expenses = expensesx
-    })
+          token: this.$store.state.user.token,
+        },
+      });
+      this.expenses = expensesx;
+    });
   },
   computed: {
-    headers () {
+    headers() {
       return [
         {
-          text: 'title',
-          align: 'start',
+          text: "title",
+          align: "start",
           sortable: false,
-          value: 'title',
+          value: "title",
         },
         {
-          text: 'date',
-          value: 'date',
-          filter: value => {
-            if (!this.date) return true
+          text: "date",
+          value: "date",
+          filter: (value) => {
+            if (!this.date) return true;
 
-            return value < this.calories
+            return value < this.calories;
           },
         },
-        { text: 'merchant', value: 'merchant' },
-        { text: 'createdat', value: 'created_at' },
-      ]
+        { text: "merchant", value: "merchant" },
+        { text: "createdat", value: "created_at" },
+      ];
     },
   },
   methods: {
-    filterOnlyCapsText (value, search, item) {
-      return value != null &&
+    filterOnlyCapsText(value, search, item) {
+      return (
+        value != null &&
         search != null &&
-        typeof value === 'string' &&
+        typeof value === "string" &&
         value.toString().toLocaleUpperCase().indexOf(search) !== -1
+      );
     },
     addNewItem() {
-      this.newExpense.expense_items.push({title: "", amount: "", price: ""})
-    }
+      this.newExpense.expense_items.push({ title: "", amount: "", price: "" });
+    },
+    async upload(event) {
+      event.preventDefault();
+      let formData = new FormData();
+      console.log(this.$refs.getFile.files[0]);
+      formData.append("thefile", this.$refs.getFile.files[0]);
+
+      const { data: response } = await this.$axios.post("/api/scan", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+
+      this.newExpense.date = response.date || this.newExpense.date;
+      this.newExpense.merchant = response.merchant || this.newExpense.merchant;
+      this.newExpense.expense_items = response.items;
+    },
   },
-}
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
