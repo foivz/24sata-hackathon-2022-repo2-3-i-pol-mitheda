8d3bcc6e28608f10ee6b7b0c14dfbd81fe1bb4c1
blekso <blekepix@outlook.com>
2022-04-03 00:22:44 +0200

    expense table

--- pages/expenses/index.vue
+++ pages/expenses/index.vue
@@ -1,19 +1,35 @@
 <template>
-  <div class="h-full w-full m-8 grid grid-cols-8 z-50">
-    <div class="col-span-8">
-     <v-data-table
-        :headers="headers"
+  <div class="h-full w-full m-8 z-50">
+    <div class="m-8 flex items-center">
+      <p
+        class="font-extrabold text-2xl mr-4 text-white"
+        style="margin-bottom: 0px"
+      >
+        All Expenses
+      </p>
+      <v-icon color="#ffffff" class="absolute">mdi-home</v-icon>
+    </div>
+    <div>
+      <v-data-table
+        :headers="tableHeaders"
         :items="expenses"
+        v-model="selectedExpenses"
         item-key="id"
         class="elevation-1 cursor-pointer"
         :search="search"
         :custom-filter="filterOnlyCapsText"
+        show-select
         show-expand
-         @click:row="(item, slot) => slot.expand(!slot.isExpanded)"
+        @click:row="(item, slot) => slot.expand(!slot.isExpanded)"
       >
-        <template v-slot:expanded-item="{item, headers}">
+        <template v-slot:expanded-item="{ item, headers }">
           <td :colspan="headers.length" style="padding: 0px">
-            <p>{{item}}</p>
+            <v-data-table
+              hide-default-footer
+              :headers="tableExpansionHeaders"
+              :items="item.expense_item"
+              class="elevation-1 w-full"
+            ></v-data-table>
           </td>
         </template>
         <template v-slot:top>
@@ -38,64 +54,93 @@
         </template>
       </v-data-table>
     </div>
+    <div class="grid grid-cols-6 gap-8 m-8">
+      <v-btn depressed color="primary" :disabled="selectedExpenses.length == 0">
+        Update
+      </v-btn>
+      <v-btn depressed color="error" :disabled="selectedExpenses.length == 0">
+        Delete
+      </v-btn>
+    </div>
   </div>
 </template>
 
 <script>
+import dayjs from "dayjs";
 export default {
-  data () {
+  data() {
     return {
-      search: '',
-      calories: '',
-      expenses: []
-    }
+      search: "",
+      calories: "",
+      expenses: [],
+      selectedExpenses: [],
+    };
   },
   async mounted() {
     this.$nextTick(async () => {
-      console.log("TOKEN:"+this.$store.state.user.token)
-      console.log()
-      const expensesx = await this.$axios.$get('/api/expenses', {
+      console.log("TOKEN:" + this.$store.state.user.token);
+      console.log();
+      const expensesx = await this.$axios.$get("/api/expenses", {
         params: {
-          token: this.$store.state.user.token
-        }
-      })
-      this.expenses = expensesx
-    })
+          token: this.$store.state.user.token,
+        },
+      });
+      this.expenses = expensesx.map((el) => {
+        el.date = this.formatDate(el.date);
+        el.created_at = this.formatDate(el.created_at);
+        return el;
+      });
+    });
   },
   computed: {
-    headers () {
+    tableHeaders() {
       return [
         {
-          text: 'title',
-          align: 'start',
+          text: "Merchant",
+          value: "merchant",
+          align: "start",
           sortable: false,
-          value: 'title',
         },
         {
-          text: 'date',
-          value: 'date',
-          filter: value => {
-            if (!this.date) return true
-
-            return value < this.calories
-          },
+          text: "Title",
+          value: "title",
         },
-        { text: 'merchant', value: 'merchant' },
-        { text: 'createdat', value: 'created_at' },
-      ]
+        {
+          text: "Date",
+          value: "date",
+        },
+        { text: "Created At", value: "created_at" },
+      ];
+    },
+    tableExpansionHeaders() {
+      return [
+        {
+          text: "Title",
+          align: "start",
+          sortable: false,
+          value: "title",
+        },
+        {
+          text: "Price",
+          value: "price",
+        },
+        { text: "Amount", value: "amount" },
+        { text: "Expense ID", value: "expense_id" },
+      ];
     },
   },
   methods: {
-    filterOnlyCapsText (value, search, item) {
-      return value != null &&
+    filterOnlyCapsText(value, search, item) {
+      return (
+        value != null &&
         search != null &&
-        typeof value === 'string' &&
+        typeof value === "string" &&
         value.toString().toLocaleUpperCase().indexOf(search) !== -1
+      );
+    },
+    formatDate(date) {
+      return dayjs(date).format("DD/MM/YYYY");
     },
   },
-}
+};
 </script>
-
-<style>
-
-</style>
