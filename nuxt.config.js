0bd461bbb14661ec6f28b70838df5218c5f7916e
leonardogazdek <bot.denobot.deno@gmail.com>
2022-04-02 11:41:55 +0200

    reg/login

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,7 +22,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    { src: '~/plugins/amplify.js', mode: 'client' }
+    { src: '~/plugins/amplify.js', ssr: 'false' }
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
