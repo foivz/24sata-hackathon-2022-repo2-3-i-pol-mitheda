3368dcdb768a440fcffa29fdc544c1ee87fd522c
blekso <blekepix@outlook.com>
2022-04-02 16:06:49 +0200

    push

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,7 +16,7 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [{ src: "~/assets/css/style.css" }],
+  css: ["~/assets/css/style.scss"],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
