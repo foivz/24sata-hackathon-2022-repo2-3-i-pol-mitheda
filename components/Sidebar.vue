0f44e73ede813aebfd253f542e68e89dd31734b1
leonardogazdek <bot.denobot.deno@gmail.com>
2022-04-03 05:41:50 +0200

    Collab

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -30,6 +30,125 @@
         </v-list-item>
       </v-list>
 
+      <v-dialog
+      v-model="dialog"
+      persistent
+      max-width="600px"
+    >
+      <template v-slot:activator="{ on, attrs }">
+
+        <v-list dense nav>
+            <v-list-item link v-bind="attrs" v-on="on">
+                <v-list-item-icon>
+                    <v-icon>mdi-logout</v-icon>
+                </v-list-item-icon>
+
+                <v-list-item-content>
+                    <v-list-item-title>Collaborate</v-list-item-title>
+                </v-list-item-content>
+            </v-list-item>
+        </v-list>
+
+
+      </template>
+      <v-card>
+        <v-card-title>
+          <span class="text-h5">User Profile</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container>
+            <v-row>
+              <v-col
+                cols="12"
+                sm="6"
+                md="4"
+              >
+                <v-text-field
+                  label="Legal first name*"
+                  required
+                ></v-text-field>
+              </v-col>
+              <v-col
+                cols="12"
+                sm="6"
+                md="4"
+              >
+                <v-text-field
+                  label="Legal middle name"
+                  hint="example of helper text only on focus"
+                ></v-text-field>
+              </v-col>
+              <v-col
+                cols="12"
+                sm="6"
+                md="4"
+              >
+                <v-text-field
+                  label="Legal last name*"
+                  hint="example of persistent helper text"
+                  persistent-hint
+                  required
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12">
+                <v-text-field
+                  label="Email*"
+                  required
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12">
+                <v-text-field
+                  label="Password*"
+                  type="password"
+                  required
+                ></v-text-field>
+              </v-col>
+              <v-col
+                cols="12"
+                sm="6"
+              >
+                <v-select
+                  :items="['0-17', '18-29', '30-54', '54+']"
+                  label="Age*"
+                  required
+                ></v-select>
+              </v-col>
+              <v-col
+                cols="12"
+                sm="6"
+              >
+                <v-autocomplete
+                  :items="['Skiing', 'Ice hockey', 'Soccer', 'Basketball', 'Hockey', 'Reading', 'Writing', 'Coding', 'Basejump']"
+                  label="Interests"
+                  multiple
+                ></v-autocomplete>
+              </v-col>
+            </v-row>
+          </v-container>
+          <small>*indicates required field</small>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="blue darken-1"
+            text
+            @click="dialog = false"
+          >
+            Close
+          </v-btn>
+          <v-btn
+            color="blue darken-1"
+            text
+            @click="dialog = false"
+          >
+            Save
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
+
+
       <template v-slot:append>
         <NuxtLink to="/logout">
           <v-list dense nav>
@@ -64,8 +183,7 @@ export default {
             items: [
             { title: 'Dashboard', icon: 'mdi-view-dashboard', route: '/', color:"green darken-2" },
             { title: 'Expenses', icon: 'mdi-image', route: '/expenses', color:"red darken-2" },
-            { title: 'Shopping List', icon: 'mdi-playlist-edit', route: '/shopping-list', color:"red darken-2" },
-            { title: 'Account', icon: 'mdi-account', route: '/user', color:"blue darken-2" },
+            { title: 'Shopping List', icon: 'mdi-playlist-edit', route: '/shopping-list', color:"red darken-2" }
         ],
     }
    
