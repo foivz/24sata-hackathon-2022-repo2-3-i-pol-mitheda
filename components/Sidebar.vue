39564d7a92adb1cda0b658b580b73210d4d13908
blekso <blekepix@outlook.com>
2022-04-03 04:19:13 +0200

    listing

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -1,154 +1,156 @@
 <template>
-    <v-card>
-        <v-navigation-drawer
-          permanent
-          expand-on-hover
-        >
+  <v-card>
+    <v-navigation-drawer permanent expand-on-hover>
+      <v-list-item class="px-2">
+        <v-list-item-avatar>
+          <v-img src="https://randomuser.me/api/portraits/women/75.jpg"></v-img>
+        </v-list-item-avatar>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6">
+            {{ $nuxt.$route.name }}
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            Logged in as: {{ userName }}
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
 
-        <v-list-item class="px-2">
-            <v-list-item-avatar>
-            <v-img src="https://randomuser.me/api/portraits/women/75.jpg"></v-img>
-            </v-list-item-avatar>
+      <v-divider></v-divider>
+      <v-list dense nav>
+        <v-list-item v-for="item in items" :key="item.title" link>
+          <NuxtLink :to="item.route" class="flex w-full">
+            <v-list-item-icon>
+              <v-icon :color="item.color">{{ item.icon }}</v-icon>
+            </v-list-item-icon>
+
             <v-list-item-content>
-            <v-list-item-title class="text-h6">
-              {{$nuxt.$route.name}}
-            </v-list-item-title>
-            <v-list-item-subtitle>
-              Logged in as: {{userName}}
-            </v-list-item-subtitle>
-          </v-list-item-content>
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item-content>
+          </NuxtLink>
         </v-list-item>
-  
-        <v-divider></v-divider>
-        <v-list
-          dense
-          nav
-        >
-          <v-list-item
-            v-for="item in items"
-            :key="item.title"
-            link
-          >
-           <NuxtLink :to="item.route" class="flex w-full">
-                <v-list-item-icon>
-                    <v-icon :color="item.color">{{ item.icon }}</v-icon>
-                </v-list-item-icon>
-        
-                <v-list-item-content>
-                    <v-list-item-title>{{ item.title }}</v-list-item-title>
-                </v-list-item-content>
-            </NuxtLink>
-          </v-list-item>
-        </v-list>
+      </v-list>
 
-        <template v-slot:append>
-            <NuxtLink to="/logout">
-                <v-list
-                dense
-                nav
-                >
-                    <v-list-item
-                    link
-                    >
-                    <v-list-item-icon>
-                        <v-icon>mdi-logout</v-icon>
-                    </v-list-item-icon>
-                    <v-list-item-content>
-                        <v-list-item-title>Logout</v-list-item-title>
-                    </v-list-item-content>
-                    </v-list-item>
-                </v-list>
-            </NuxtLink>
-        </template>
-        </v-navigation-drawer>
-    </v-card>
+      <template v-slot:append>
+        <NuxtLink to="/logout">
+          <v-list dense nav>
+            <v-list-item link>
+              <v-list-item-icon>
+                <v-icon>mdi-logout</v-icon>
+              </v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title>Logout</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+        </NuxtLink>
+      </template>
+    </v-navigation-drawer>
+  </v-card>
 </template>
 
 <script>
 export default {
-    computed: {
-        userName() {
-            return this.$store.state.user.userObj?.accessToken?.payload?.username ?? "ERROR"
-        }
+  computed: {
+    userName() {
+      return (
+        this.$store.state.user.userObj?.accessToken?.payload?.username ??
+        "ERROR"
+      );
     },
-    data() {
-      return {
-          sidebarExpanded: true,
-          items: [
-            { title: 'Dashboard', icon: 'mdi-view-dashboard', route: '/', color:"green darken-2" },
-            { title: 'Expenses', icon: 'mdi-image', route: '/expenses', color:"red darken-2" },
-            { title: 'Account', icon: 'mdi-account', route: '/user', color:"blue darken-2" },
-        ],
-      }
-   
-  }
+  },
+  data() {
+    return {
+      sidebarExpanded: true,
+      items: [
+        {
+          title: "Dashboard",
+          icon: "mdi-view-dashboard",
+          route: "/",
+          color: "green darken-2",
+        },
+        {
+          title: "Expenses",
+          icon: "mdi-image",
+          route: "/expenses",
+          color: "red darken-2",
+        },
+        {
+          title: "Account",
+          icon: "mdi-account",
+          route: "/user",
+          color: "blue darken-2",
+        },
+        {
+          title: "Listing",
+          icon: "mdi-paperclip",
+          route: "/listing",
+          color: "purple darken-2",
+        },
+      ],
+    };
+  },
 };
 </script>
 
 <style lang="scss">
 .sidebar {
-    position: relative;
-    background: #fff;
-    height: 100vh;
-    box-shadow: 0 0 2rem 0 rgb(136 152 170 / 15%);
+  position: relative;
+  background: #fff;
+  height: 100vh;
+  box-shadow: 0 0 2rem 0 rgb(136 152 170 / 15%);
 
-    transition: width .3s linear;
+  transition: width 0.3s linear;
 
-    .openSidebar {
-        float: right;
-        margin: 10px;
-    }
+  .openSidebar {
+    float: right;
+    margin: 10px;
+  }
 
-    width: 50px;
+  width: 50px;
 
-    .sidebar-btn {
-        white-space: normal;
-    }
-    .sidebar-text {
-        display: none;
-        vertical-align: middle;
-    }
+  .sidebar-btn {
+    white-space: normal;
+  }
+  .sidebar-text {
+    display: none;
+    vertical-align: middle;
+  }
 
-    .sidebarIco {
-        vertical-align: middle;
-    }
+  .sidebarIco {
+    vertical-align: middle;
+  }
 
-    .sidebarBottom {
-        position: absolute;
-        bottom: 10px;
-        left: 0;
-        right: 0;
-
-    }
+  .sidebarBottom {
+    position: absolute;
+    bottom: 10px;
+    left: 0;
+    right: 0;
+  }
 }
-
 
 .sidebar {
-    &.expanded {
-        width: 250px;
+  &.expanded {
+    width: 250px;
 
-        .sidebar-btn {
-            white-space: nowrap;
-        }
-        .sidebar-text {
-            display: inline;
-        }
+    .sidebar-btn {
+      white-space: nowrap;
     }
-    
+    .sidebar-text {
+      display: inline;
+    }
+  }
 }
 .sidebar-btn {
-    display: block;
-    text-align: left;
-    font-family: "Open Sans";
-    padding: 10px 15px;
-    
+  display: block;
+  text-align: left;
+  font-family: "Open Sans";
+  padding: 10px 15px;
 
-
-    .sidebarIco {
-        color: #fff;
-        margin-right: 5px;
-        display: inline-block;
-        width: 28px;
-    }
+  .sidebarIco {
+    color: #fff;
+    margin-right: 5px;
+    display: inline-block;
+    width: 28px;
+  }
 }
 </style>
---
+++ pages/listing/index.vue
@@ -0,0 +1,130 @@
+<template>
+  <div class="h-full w-full m-8 z-50">
+    <div class="m-8 flex items-center">
+      <p
+        class="font-extrabold text-2xl mr-4 text-white"
+        style="margin-bottom: 0px"
+      >
+        Add listing
+      </p>
+      <v-icon color="#ffffff" class="absolute">mdi-home</v-icon>
+    </div>
+    <div class="grid gap-4">
+      <v-card elevation="2" class="p-8">
+        <p class="text-xl" style="margin-bottom: 0px">
+          You can list your product here and we are going to recommend it to
+          users while they are creating their shopping list.
+        </p>
+      </v-card>
+      <v-card elevation="2" class="p-8">
+        <p class="text-xl" style="margin-bottom: 0px">
+          Your listing will be confirmed once we receive a deposit.
+        </p>
+      </v-card>
+      <v-card elevation="2" class="p-8">
+        <v-form ref="form" v-model="valid" lazy-validation>
+          <v-text-field
+            v-model="name"
+            :counter="10"
+            :rules="nameRules"
+            label="Name"
+            required
+          ></v-text-field>
+
+          <v-textarea
+            v-model="description"
+            :rules="descriptionRules"
+            label="Description"
+            required
+          >
+          </v-textarea>
+
+          <v-text-field
+            v-model="address"
+            :rules="descriptionRules"
+            label="Address"
+            required
+          ></v-text-field>
+
+          <v-checkbox v-model="freeDelivery" label="Free delivery"></v-checkbox>
+
+          <v-text-field
+            v-model="paymentInfo"
+            :rules="descriptionRules"
+            label="Payment info"
+            required
+          ></v-text-field>
+
+          <v-text-field
+            v-model="nekiBrojIdkLol"
+            :rules="descriptionRules"
+            label="SW,FT/BIC"
+            required
+          ></v-text-field>
+
+          <v-text-field
+            v-model="iban"
+            :rules="descriptionRules"
+            label="IBAN"
+            required
+          ></v-text-field>
+
+          <v-snackbar v-model="snackbar">
+            {{ text }}
+
+            <template v-slot:action="{ attrs }">
+              <v-btn color="pink" text v-bind="attrs" @click="snackbar = false">
+                Close
+              </v-btn>
+            </template>
+          </v-snackbar>
+
+          <v-btn color="success" class="mr-4 mt-4" @click="validate">
+            Send
+          </v-btn>
+        </v-form>
+      </v-card>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    valid: true,
+    name: "",
+    nameRules: [
+      (v) => !!v || "Name is required",
+      (v) => (v && v.length <= 10) || "Name must be less than 10 characters",
+    ],
+    description: "",
+    descriptionRules: [(v) => !!v || "Description is required"],
+    address: "",
+    freeDelivery: false,
+    paymentInfo: "",
+    nekiBrojIdkLol: "",
+    iban: "",
+    snackbar: false,
+    text: `Success`,
+  }),
+
+  methods: {
+    validate() {
+      this.$refs.form.validate();
+      setTimeout(() => {
+        if (this.valid) {
+          this.snackbar = true;
+        }
+      }, 1000);
+    },
+    reset() {
+      this.$refs.form.reset();
+    },
+    resetValidation() {
+      this.$refs.form.resetValidation();
+    },
+  },
+};
+</script>
+
+<style></style>
