c354577e7c98b75cfc963b463ae5028903f67ad2
blekso <blekepix@outlook.com>
2022-04-03 07:26:14 +0200

    sodk

--- pages/shopping-list/index.vue
+++ pages/shopping-list/index.vue
@@ -35,7 +35,7 @@
         <template v-slot:top>
           <v-text-field
             v-model="search"
-            label="Search (UPPER CASE ONLY)"
+            label="Search"
             class="mx-4"
           ></v-text-field>
         </template>
